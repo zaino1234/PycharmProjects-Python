0a0f0931594093f8ea3a6ee37b3e07e1e2b2b7ae
Renat <renato00091@live.com>
2021-07-31 18:57:11 -0300

    ga

--- CursoemVídeo/ex008.py
+++ CursoemVídeo/ex008.py
@@ -1,4 +1,5 @@
 comprimneto = float(input('Digite a Distância em Metros: '))
 cm = comprimneto * 100
 mm = comprimneto * 1000
-print(f'A medida de {comprimneto}m corresponde a {cm}cm \n e {mm}mm')
+print(f'A medida de {comprimneto:.0f} m corresponde a '
+      f'{cm:.0f} cm e {mm:.0f} mm')
