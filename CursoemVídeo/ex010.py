c16a7f496c41f1cf67a65feac358c9d8ce91ccaa
Renat <renato00091@live.com>
2021-07-31 19:31:48 -0300

    ga

---
+++ CursoemVídeo/ex010.py
@@ -0,0 +1,7 @@
+#valor considerado do dolar 5,21 em 31-07-2021
+print('-'*40)
+print(f'{"Conversor de REAL (R$) em DOLAR ($)":^40}')
+print('-'*40)
+real = float(input('Valor em REAIS R$: '))
+print(f'Com R$ {real} você consegue comprar $ {real/5.21:.2f}')
+print('-'*40)
