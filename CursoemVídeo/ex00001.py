25a9fdd2b9776b7922f72e3179b34b7b9dbe720b
Renat <renato00091@live.com>
2021-07-31 17:14:54 -0300

    feito

--- CursoemVídeo/ex00001.py
+++ CursoemVídeo/ex00001.py
@@ -1,4 +1,15 @@
-print('Teste de Alteração')
-
-
-
+from random import randint
+n = randint(1, 10)
+while True:
+    escola = int(input('Escola um número ente 0 a 10: '))
+    if n == escola:
+        print('Sua resposta foi correta')
+        break
+    resp = input('Quer tentar novamente? ').strip().upper()[0]
+    if resp == 'S':
+        if n > escola:
+            print(f'Maior')
+        if n < escola:
+            print(f'Menor')
+    if resp == 'N':
+        break
