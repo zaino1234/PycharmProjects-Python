477910a25c06a9a7c40c032d26ce8ccc709f06ea
Renat <renato00091@live.com>
2021-08-01 15:07:49 -0300

    31-07

--- CursoemVídeo/ex012.py
+++ CursoemVídeo/ex012.py
@@ -5,3 +5,4 @@ valorpro = float(input('Qual é o preço do produto? R$ '))
 des5 = valorpro - valorpro*5/100
 print(f'O produto que custava R$ {valorpro},'
       f' na promoção com desconto de 5% vai custar R${des5}')
+f
