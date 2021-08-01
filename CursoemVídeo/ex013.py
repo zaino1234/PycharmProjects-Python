7668afa120cc74af8d92ee73722c4030691fb6ed
Renat <renato00091@live.com>
2021-08-01 15:19:36 -0300

    31-07

--- CursoemVídeo/ex013.py
+++ CursoemVídeo/ex013.py
@@ -1,1 +1,7 @@
 print('x'*40)
+print(f'{"NOVOS SALÁRIOS":^40}')
+print('x'*40)
+salario = float(input('Qual é o salário atual? '))
+aum15 = salario + (salario*15/100)
+print(f'Um funcionário que ganhava R${salario}, com 15%'
+      f' de aumento, passa a receber R${aum15:.2f}')
