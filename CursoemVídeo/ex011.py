36a941386746b1970fc8c785525ae2831fe52e68
Renat <renato00091@live.com>
2021-07-31 23:03:23 -0300

    31-07

---
+++ CursoemVídeo/ex011.py
@@ -0,0 +1,15 @@
+print('-'*40)
+print(f'{"Calculadora do Valor da Tinta":^40}')
+print('-'*40)
+# cada litro de tinta pinta uma área de 2 metros quadrado
+largura = float(input('Largura da Parede: m '))
+altura = float(input('Altura da Parede: m '))
+area = largura*altura
+print(
+    f'Sua parede tem a dimensão de {largura}m x {altura}m e '
+      f'sua área é de {area:.2f}m²'
+)
+print(
+    f'Para pintar essa parece, você precisa de '
+    f'{area/2:.2f}l de tinta'
+)
