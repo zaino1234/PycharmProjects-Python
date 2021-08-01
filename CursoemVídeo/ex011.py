07b8377846e9e7799c1ccc20399a2ab3a776a81b
Renat <renato00091@live.com>
2021-07-31 23:24:11 -0300

    31-07

--- CursoemVídeo/ex011.py
+++ CursoemVídeo/ex011.py
@@ -1,29 +1,12 @@
 print('-'*40)
-<<<<<<< HEAD
 print(f'{"Calculadora do Valor da Tinta":^40}')
-=======
-print(f'{"Calculador Valor da Tinta":^40}')
->>>>>>> origin/master
 print('-'*40)
 # cada litro de tinta pinta uma área de 2 metros quadrado
 largura = float(input('Largura da Parede: m '))
 altura = float(input('Altura da Parede: m '))
-<<<<<<< HEAD
 area = largura*altura
-print(
-    f'Sua parede tem a dimensão de {largura}m x {altura}m e '
-      f'sua área é de {area:.2f}m²'
-)
-print(
-    f'Para pintar essa parece, você precisa de '
-    f'{area/2:.2f}l de tinta'
-=======
-print(
-    f'Sua parede tem a dimensão de {largura}m x {altura}m e '
-      f'sua área é de {largura*altura:.2f}m²'
-)
-print(
-    f'Para pintar essa parece, você precisa de '
-    f'{(largura*altura)/2:.2f}l de tinta'
->>>>>>> origin/master
-)
+print(f'Sua parede tem a dimensão de {largura}m x {altura}m e '
+      f'sua área é de {area:.2f}m²')
+print(f'Para pintar essa parece, você precisa de '
+    f'{area/2:.2f}l de tinta')
+
