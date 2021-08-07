5d4a0f7aa1c940e119a283684806da874d2d0df7
Renat <renato00091@live.com>
2021-08-07 14:24:02 -0300

    ga

--- CursoemVídeo/ex014.py
+++ CursoemVídeo/ex014.py
@@ -6,7 +6,9 @@ opcao = int(input('''[ 1 ] Celsius (°C) para Fahrenheit(°F)
 Escola uma opção: '''))
 if opcao == 1:
     TC = float(input('Digite a temperatura: °C '))
-    Tf = ((TC * 9 / 5) + 32)
-    print(f'{TC:.1f}°C = {Tf:.1f}°F')
+    TF = ((TC * 9 / 5) + 32)
+    print(f'{TC}°C = {TF}°F')
 if opcao == 2:
-    tff = float(input('Digite a temperatura: °F '))
+    TF = float(input('Digite a temperatura: °F '))
+    TC = ((TF - 32)*5)/9
+    print(f'{TF}°F = {TC}°C')
