eeaec8a1a6ada0d89987863382b102faad371a10
Renat <renato00091@live.com>
2021-08-09 17:16:09 -0300

    ga

---
+++ Aplicações/Analise de Lista no Excel + Enviar msg.py
@@ -0,0 +1,25 @@
+import pandas as pd# Integração do python excel
+import openpyxl as op
+import twilio as tw# Integração do python com sms
+from twilio.rest import Client
+
+lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+for mes in lista_meses:
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+
+    if (tabela_vendas['Vendas'] > 55000).any():# indica a coluna escolhida na tabela tabela_vendas ['Vendas']
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f'O vendedor {vendedor} vendeu R$ {vendas} no mês de {mes}.')
+
+        # pega o account_sid no site auth_token
+        account_sid = "AC8cd2b2da3bdbe71b6a10a4c85cd5a69c"
+        auth_token  = "38bd46ce4144b9b3dcd1aea08713e4e3"
+
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+            to="+5561993502389",
+            from_="+15046081225",
+            body=f'O vendedor {vendedor} vendeu R$ {vendas} no mês de {mes}.')
+        print(message.sid)
