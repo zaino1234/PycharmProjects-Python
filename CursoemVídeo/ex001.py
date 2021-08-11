36a5421cf7ddd29252abe7288bd114e710631d47
Renat <renato00091@live.com>
2021-08-10 22:52:25 -0300

    ga

--- CursoemVídeo/ex001.py
+++ CursoemVídeo/ex001.py
@@ -1,13 +1,13 @@
 from random import randint
 n = randint(1, 10)
 while True:
-    escola = int(input('Escola um número ente 0 a 10: '))
-    if n == escola:
+    escolha = int(input('Escolha um número ente 0 a 10: '))
+    if n == escolha:
         print(f'Parabéns!!!'
               f'Sua resposta foi correta...'
               f' eu tinha escolhido {n}')
         break
-    if n > escola:
+    if n > escolha:
         print(f'Maior')
-    if n < escola:
+    if n < escolha:
         print(f'Menor')
